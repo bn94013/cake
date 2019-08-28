676efbcf1f26b8a2e5bbf98087cbf520ca47a35f
bn94013 <bn94013@gmail.com>
2019-08-28 23:10:21 +0800

    0
    00

--- js/index.js
+++ js/index.js
@@ -25,17 +25,17 @@ $(document).ready(function () {
 
 function onScroll(event) {
 	var scrollPos = $(document).scrollTop();
-	$('.menu-main li a').each(function () {
-		var currLink = $(this);
-		var refElement = $(currLink.attr("href"));
-		if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-			$('.menu-main li a').removeClass("active");
-			currLink.addClass("active");
-		}
-		else {
-			currLink.removeClass("active");
-		}
-	});
+	// $('.menu-main li a').each(function () {
+	// 	var currLink = $(this);
+	// 	var refElement = $(currLink.attr("href"));
+	// 	if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+	// 		$('.menu-main li a').removeClass("active");
+	// 		currLink.addClass("active");
+	// 	}
+	// 	else {
+	// 		currLink.removeClass("active");
+	// 	}
+	// });
 }
 
 
