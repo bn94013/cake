de32655c0ed2fe92773e91f82c668d9b6fc3e763
bn94013 <bn94013@gmail.com>
2019-08-28 22:52:00 +0800

    0828
    0828

---
+++ js/all.js
@@ -0,0 +1,85 @@
+
+document.querySelector('.gocake_1').onclick =  function() {
+    alert('已添加到購物車');
+};
+
+
+document.querySelector('.gocake_2').onclick =  function(){
+    alert('已添加到購物車');
+};
+    
+document.querySelector('.gocake_3').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gocake_4').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gocake_5').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gocake_6').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gocake_7').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gocake_8').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.goegg_1').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.goegg_2').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.goegg_3').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.goegg_4').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gobaking_1').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gobaking_2').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gobaking_3').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gobaking_4').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gobaking_5').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gobaking_6').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gobaking_7').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.gobaking_8').onclick =  function(){
+    alert('已添加到購物車');
+};
+
+document.querySelector('.senda').onclick =  function(){
+    alert('感謝您的寶貴意見!');
+};
---
+++ js/index.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+	$(document).on("scroll", onScroll);
+
+	//smoothscroll
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
+		$(document).off("scroll");
+
+		$('a').each(function () {
+			$(this).removeClass('active');
+		})
+		$(this).addClass('active');
+
+		var target = this.hash,
+			menu = target;
+		$target = $(target);
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top + 0
+		}, 500, 'swing', function () {
+			window.location.hash = target;
+			$(document).on("scroll", onScroll);
+		});
+	});
+});
+
+function onScroll(event) {
+	var scrollPos = $(document).scrollTop();
+	$('.menu-main li a').each(function () {
+		var currLink = $(this);
+		var refElement = $(currLink.attr("href"));
+		if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+			$('.menu-main li a').removeClass("active");
+			currLink.addClass("active");
+		}
+		else {
+			currLink.removeClass("active");
+		}
+	});
+}
+
+
+
+
+
+
+// 等三秒後跳出視窗
+window.onload = function () {
+	time = window.setInterval("showc()", 3000);
+}
+function showc() {
+	var adDiv = document.getElementById("adDiv");
+	adDiv.style.display = "block";
+}
+
+function ww2() {
+	document.getElementById('abox1').style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
